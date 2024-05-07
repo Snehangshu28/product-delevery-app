0ab6b1b9542e0df2537f16d72ea9ef743478b210
snehangshu <snehangshu1228@gmail.com>
2024-05-08 01:17:23 +0530

    home menu add

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from './component/layout/Header';
 import Hero from './component/layout/Hero';
+import { HomeMenu } from './component/layout/HomeMenu';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <HomeMenu />
     </>
   );
 }
